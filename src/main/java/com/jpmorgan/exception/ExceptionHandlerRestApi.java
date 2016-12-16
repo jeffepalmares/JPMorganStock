38995c071272054be61653bcda3d6eb42dfeac35
Jefferson Sancao Cardoso <12679@cnoutmacweb02.local>
2016-12-16 08:31:31 -0200

    Exception handler

--- src/main/java/com/jpmorgan/exception/ExceptionHandlerRestApi.java
+++ src/main/java/com/jpmorgan/exception/ExceptionHandlerRestApi.java
@@ -2,6 +2,7 @@ package com.jpmorgan.exception;
 
 import org.apache.log4j.Logger;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -13,16 +14,19 @@ public class ExceptionHandlerRestApi {
 	
 	@ResponseStatus(value=HttpStatus.EXPECTATION_FAILED ,  code = HttpStatus.EXPECTATION_FAILED )
 	@ExceptionHandler(BusinessException.class)
-	public String businessException(BusinessException ex){
+	public ResponseEntity<String> businessException(BusinessException ex){
 		logger.warn( ex.getMessage() );
-		return ex.getMessage();
+		
+		return new ResponseEntity<String>( ex.getMessage(), HttpStatus.EXPECTATION_FAILED ); 
 	}
 
 	@ResponseStatus(value=HttpStatus.INTERNAL_SERVER_ERROR)
 	@ExceptionHandler(Exception.class)
-	public String exception(Exception ex){
+	public ResponseEntity<String> exception(Exception ex){
+		
 		logger.error( ex.getMessage() );
-		return "Ops.. So Sorry bu something went wrong, please try again!";
+		
+		return new ResponseEntity<String>( "Ops.. So Sorry bu something went wrong, please try again!" , HttpStatus.EXPECTATION_FAILED );
 	}
 
 }
